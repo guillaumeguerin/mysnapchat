f390768626f2220d1cb0a62259104ce2c909ce7c
gmeral <guillaume.meral@etu.u-bordeaux1.fr>
2014-02-19 15:22:15 +0100

    premiers fichiers Doctrine

---
+++ doctrineORM/src/user.php
@@ -0,0 +1,27 @@
+<?php
+ /**
+ * @Entity @Table(name="users")
+ **/
+class User
+{
+   
+     /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+    /** @Column(type="string") **/
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
