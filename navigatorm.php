2c6b9897176dac68e5fec507e76643db97af75c0
Julio Matarranz <jumatarr@emi.u-bordeaux1.fr>
2014-03-07 11:37:37 +0100

    suppression fichier devenu inutile

--- navigatorm.php
+++
@@ -1,7 +0,0 @@
-<center>
-    <label id="newm">Create new message</label><br><br>
-    <a id="navmt" href="submitMessage.php?type=text" class="fa fa-comment-o fa-2x" style="color:rgb(201, 195, 195);"></a>
-    <a id="navmp" href="submitMessage.php?type=picture" class="fa fa-camera fa-2x" style="margin-left:2em;color:rgb(201, 195, 195);"></a>
-    <a id="navmv" href="submitMessage.php?type=video" class="fa fa-film fa-2x" style="margin-left:2em;color:rgb(201, 195, 195);"></a>
-    <a id="navmm" href="submitMessage.php?type=music" class="fa fa-music fa-2x" style="margin-left:2em;color:rgb(201, 195, 195);"></a>
-</center>
