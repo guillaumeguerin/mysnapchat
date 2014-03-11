fccc3885a067f75e799daff83c99c988c9624cb6
Guillaume Guerin De Tourville <gguerind@gmail.com>
2014-03-11 19:10:01 +0100

    suppression du dossier php

--- my-app/www/php/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysql_connect("sql301.0fees.net","fees0_14337243","j3qxtbh9");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-
-mysql_select_db("fees0_14337243_snapchat",$con);
-?> 
